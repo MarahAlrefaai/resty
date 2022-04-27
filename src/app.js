651ef213761810ae0f3ad334fa864cc2eaeed63d
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-04-27 13:31:32 +0300

    done

--- src/app.js
+++
@@ -1,6 +0,0 @@
-import Content from './component/content'
-function App(){
-  return(
-  <Content texttoren='hello from app'/>
-)}
-export default App;
--- src/component/content.js
+++
@@ -1,9 +0,0 @@
-const content =(props)=>{
-  return(
-    <>
-    <h1>hello from content </h1>
-    <p>{props.texttoren}</p>
-    </>
-  )
-}
-export default content;
